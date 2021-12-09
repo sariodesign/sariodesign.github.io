79b6ff8fc313ce34d2d6c34d2be0c91296e7119b
Ros <ros@iMac-di-ciaopeople.local>
2021-12-09 10:45:18 +0100

    Add data attribute for time reference

--- time-tracking-dashboard/js/app.js
+++ time-tracking-dashboard/js/app.js
@@ -1,3 +1,9 @@
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// fetch('./data.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+const timeReference = document.querySelectorAll('.js-time-reference');
+
+timeReference.forEach( time => {
+  time.addEventListener('click', (e) => console.log(e.target.dataset));
+})

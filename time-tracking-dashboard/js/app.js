7c8f61a57e8c024ddff5b311906ce1783e9b94d2
Ros <ros@iMac-di-ciaopeople.local>
2021-12-09 10:50:03 +0100

    Updated js

--- time-tracking-dashboard/js/app.js
+++ time-tracking-dashboard/js/app.js
@@ -1,9 +1,12 @@
-// fetch('./data.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
 
 const timeReference = document.querySelectorAll('.js-time-reference');
 
 timeReference.forEach( time => {
-  time.addEventListener('click', (e) => console.log(e.target.dataset));
+  time.addEventListener('click', (e) => {
+    let timeReference = e.target.dataset.timeRef;
+    console.log(timeReference);
+  });
 })

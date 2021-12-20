1c99247466f40813d8343f97a415be3296e047c2
Rosario <sariodesign.it@gmail.com>
2021-12-20 21:39:17 +0100

    Updated tip-calculator

--- tip-calculator-app/js/app.js
+++ tip-calculator-app/js/app.js
@@ -1,16 +1,43 @@
 const tipValues = document.querySelectorAll('.tip-value');
+const errorBillMsg = 'Add valid value for bill';
+
+bill.addEventListener('change', (e) => {
+  if(bill.value && bill.value != 0){
+    tipValues.forEach( tip => {
+      tip.disabled = false;
+    });
+    reset.disabled = false;
+  } else {
+    tipValues.forEach( tip => {
+      tip.disabled = true;
+    });
+    errorBill.textContent = errorBillMsg;
+  }
+})
 
 tipValues.forEach( tip => {
   tip.addEventListener('change', (e) => {
-    let billValue = Number(bill.value);
-    let tipValue = Number(e.target.value);
-    let totalPeople = Number(people.value);
-    console.log('Tip value: ', calcTip(billValue, tipValue));
-    console.log('Total with tip value: ', calcTotal(billValue, calcTip(billValue, tipValue)));
-    console.log('Total tip per people: ', calcTipPeople(calcTip(billValue, tipValue), totalPeople));
-    console.log('Total with tip per people: ', calcTotalPeople(calcTotal(billValue, calcTip(billValue, tipValue)), totalPeople));
+    removeClass(tipValues, 'selected');
+    if(e.target.value){
+      people.disabled = false;
+      tip.classList.add('selected');
+    }
   }) 
 });
+
+people.addEventListener('change', (e) => {
+  let billValue = Number(bill.value);
+  let tipValue =  Number(document.querySelector('.selected').value);
+  let totalPeople = Number(e.target.value);
+  let totalTip = calcTipPeople(calcTip(billValue, tipValue), totalPeople);
+  let total = calcTotalPeople(calcTotal(billValue, calcTip(billValue, tipValue)), totalPeople);
+  totalTipForPeople.textContent = totalTip.toFixed(2);
+  totalForPeople.textContent = total.toFixed(2);
+});
+
+const removeClass = (element, className) => {
+  element.forEach( item => item.classList.remove(className));
+}
 
 const calcTip = (billVal, tipVal) => {
   return billVal * tipVal / 100;

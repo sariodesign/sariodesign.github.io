7a47ec796bbe5a580cc05a9d5be9a27781b60037
Ros <ros@iMac-di-ciaopeople.local>
2022-01-12 18:32:49 +0100

    Update tip calculator

--- tip-calculator-app/js/app.js
+++ tip-calculator-app/js/app.js
@@ -1,6 +1,18 @@
 const tipValues = document.querySelectorAll('.tip-value');
 const errorBillMsg = 'Add valid value for bill';
 
+const emptyValue = (e) => {
+  e.value = '';
+}
+
+bill.addEventListener('click', () => {
+  emptyValue(bill);
+});
+
+person.addEventListener('click', () => {
+  emptyValue(person);
+});
+
 bill.addEventListener('change', (e) => {
   if(bill.value && bill.value != 0){
     tipValues.forEach( tip => {
@@ -11,6 +23,7 @@ bill.addEventListener('change', (e) => {
     tipValues.forEach( tip => {
       tip.disabled = true;
     });
+    console.log('Not a valid valueß');
     errorBill.textContent = errorBillMsg;
   }
 })
@@ -19,21 +32,30 @@ tipValues.forEach( tip => {
   tip.addEventListener('change', (e) => {
     removeClass(tipValues, 'selected');
     if(e.target.value){
-      people.disabled = false;
+      person.disabled = false;
       tip.classList.add('selected');
     }
   }) 
 });
 
-people.addEventListener('change', (e) => {
-  let billValue = Number(bill.value);
-  let tipValue =  Number(document.querySelector('.selected').value);
-  let totalPeople = Number(e.target.value);
-  let totalTip = calcTipPeople(calcTip(billValue, tipValue), totalPeople);
-  let total = calcTotalPeople(calcTotal(billValue, calcTip(billValue, tipValue)), totalPeople);
-  totalTipForPeople.textContent = totalTip.toFixed(2);
-  totalForPeople.textContent = total.toFixed(2);
+person.addEventListener('keyup', (e) => {
+  if(e.target.value){
+    let billValue = Number(bill.value);
+    let tipValue =  Number(document.querySelector('.selected').value);
+    let totalPerson = Number(e.target.value);
+    let totalTip = calcTipPerson(calcTip(billValue, tipValue), totalPerson);
+    let total = calcTotalPerson(calcTotal(billValue, calcTip(billValue, tipValue)), totalPerson);
+    totalTipForPerson.textContent = totalTip.toFixed(2);
+    totalForPerson.textContent = total.toFixed(2);
+  }
 });
+
+reset.addEventListener('click', () => {
+  bill.value = '0';
+  person.value = '0';
+  totalTipForPerson.textContent = '0.00';
+  totalForPerson.textContent = '0.00';
+})
 
 const removeClass = (element, className) => {
   element.forEach( item => item.classList.remove(className));
@@ -47,10 +69,10 @@ const calcTotal = (bill, tip) => {
   return bill + tip
 }
 
-const calcTipPeople = (totalTip, people) => {
-  return totalTip / people
+const calcTipPerson = (totalTip, person) => {
+  return totalTip / person
 }
 
-const calcTotalPeople = (total, people) => {
-  return total / people
+const calcTotalPerson = (total, person) => {
+  return total / person
 }

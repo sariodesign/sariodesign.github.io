5820e14f17694c873cce53699ead29eb29d49df2
Ros <ros@iMac-di-ciaopeople.local>
2022-02-17 14:49:46 +0100

    Update mainjs

--- coin/main.js
+++ coin/main.js
@@ -1,4 +1,8 @@
 console.log('[JS LOADED]')
 
+const API_URL = 'https://api.coinbase.com/v2/prices/:currency_pair/buy';
 
+fetch(API_URL)
+  .then(response => response.json())
+  .then(data => console.log(data));
 

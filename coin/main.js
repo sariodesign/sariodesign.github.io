7e9d699ef73ad018f5390595f88012f66db57a35
Ros <ros@iMac-di-ciaopeople.local>
2022-02-17 14:58:29 +0100

    Remove mainjs

--- coin/main.js
+++ coin/main.js
@@ -1,8 +1,3 @@
 console.log('[JS LOADED]')
 
-const API_URL = 'https://api.coinbase.com/v2/prices/:currency_pair/buy';
-
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => console.log(data));
 

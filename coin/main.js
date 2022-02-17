4f5a59bdb7a947cc07b60c1fabb4f5b9bbd87f6e
Ros <ros@iMac-di-ciaopeople.local>
2022-02-17 12:41:25 +0100

    Update fetch call

--- coin/main.js
+++ coin/main.js
@@ -2,7 +2,26 @@ console.log('[JS LOADED]')
 
 const API_URL = 'https://api.coinbase.com/v2/';
 
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => console.log(data));
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  postData(API_URL)
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
 
